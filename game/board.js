fad98563daf49b73c4e989fe2b451816bceea9ae
Giulliano Guimarães <llthguimaraes@gmail.com>
2023-10-31 17:00:17 -0300

    squares

--- game/board.js
+++ game/board.js
@@ -1,8 +1,23 @@
-import { columns, indicatorColumns, indicatorRanks, ranks } from "./variables.js";
+import { board, columns, indicatorColumns, indicatorRanks, isEven, isOdd, ranks, setSquare } from "./variables.js";
+
+const squareElement = (square, columnIndex, rankIndex) => {
+    let element = document.createElement("div")
+    let castle = square === "c1" || square === "g1" || square === "c8" || square === "g8" ? " castle" : ""
+
+    let squareColor = (isEven(columnIndex) && isEven(rankIndex)) || (isOdd(columnIndex) && isOdd(rankIndex))
+        ? "light" : "dark"
+
+    element.className = `square ${squareColor}${castle}`
+    setSquare(element, square)
+
+    return element
+}
 
 export default function Board() {
     columns.forEach((column, columnIndex) => {
         indicatorColumns.innerHTML += `<span>${column}</span>`
         indicatorRanks.innerHTML += `<span>${ranks[columnIndex]}</span>`
+
+        ranks.forEach((rank, rankIndex) => board.append(squareElement(column + rank, columnIndex, rankIndex)))
     })
 }
--- game/variables.js
+++ game/variables.js
@@ -1,8 +1,19 @@
+// DOM
 export const getElement = (selector, parent = null) => (parent ?? document).querySelector(selector)
 export const getElements = (selector, parent = null) => (parent ?? document).querySelectorAll(selector)
 
-export const indicatorColumns = getElement(".indicator.columns")
-export const indicatorRanks = getElement(".indicator.ranks")
+// SOME OPERATIONS
+export const isEven = (number) => number % 2 === 0
+export const isOdd = (number) => !isEven(number)
 
+// BOARD
+export const board = getElement(".board")
+export const indicatorColumns = getElement(".indicator.columns", board)
+export const indicatorRanks = getElement(".indicator.ranks", board)
+
+// SQUARES
 export const columns = ["a", "b", "c", "d", "e", "f", "g", "h"]
 export const ranks = ["1", "2", "3", "4", "5", "6", "7", "8"].reverse()
+
+// GAME OPERATIONS
+export const setSquare = (element, square) => element.setAttribute("data-square", square)
